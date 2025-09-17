c6fc1c9ec90d5aa9f7f3d510a96fa2459d847e01
Hardik-Ashra <hardikashra1234@gmail.com>
2025-09-17 20:50:32 +0530

    revert changes

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -9,12 +9,12 @@ import { OAuthProvider } from "node-appwrite";
 export async function signUpWithGithub() {
 	const { account } = await createAdminCleint();
 
-//   const origin = (await headers()).get("origin");
+  const origin = (await headers()).get("origin");
 
 	const redirectUrl = await account.createOAuth2Token(
 		OAuthProvider.Github,
-		`${process.env.NEXT_PUBLIC_APP}/oauth`,
-		`${process.env.NEXT_PUBLIC_APP}/sign-up`
+		`${origin}/oauth`,
+		`${origin}/sign-up`
 	);
 
 	return redirect(redirectUrl);
@@ -26,8 +26,8 @@ export async function signUpWithGoogle() {
 
 	const redirectUrl = await account.createOAuth2Token(
 		OAuthProvider.Google,
-		`${process.env.NEXT_PUBLIC_APP}/oauth`,
-		`${process.env.NEXT_PUBLIC_APP}/sign-up`
+		`${origin}/oauth`,
+		`${origin}/sign-up`
 	);
 
 	return redirect(redirectUrl);
